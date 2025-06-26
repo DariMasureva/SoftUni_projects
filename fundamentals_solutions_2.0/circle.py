990388c9681bf2632a26ea767a00ff6405f3fe0a
Darina Masureva <darimasureva@gmail.com>
2025-06-26 19:46:41 +0300

    added solutions

---
+++ fundamentals_solutions_2.0/circle.py
@@ -0,0 +1,24 @@
+class Circle:
+    __pi = 3.14
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+        self.radius = diameter / 2
+
+    def calculate_circumference(self):
+        circumference = Circle.__pi * self.diameter
+        return circumference
+
+    def calculate_area(self):
+        area = Circle.__pi * (self.radius ** 2)
+        return area
+
+    def calculate_area_of_sector(self, angle):
+        sector_area = (angle/360) * Circle.__pi * (self.radius ** 2)
+        return sector_area
+
+circle = Circle(int(input()))
+angle = int(input())
+print(circle.calculate_circumference())
+print(circle.calculate_area())
+print(circle.calculate_area_of_sector(angle))
