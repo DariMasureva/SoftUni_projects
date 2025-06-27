d249509ef12e46c0867c880208ca2d1f26687000
Darina Masureva <darimasureva@gmail.com>
2025-06-27 18:14:28 +0300

    added solutions

--- fundamentals_solutions_2.0/catalogue.py
+++ fundamentals_solutions_2.0/catalogue.py
@@ -1,17 +1,18 @@
 class Catalogue:
-    products = []
 
     def __init__(self, name:str):
         self.name = name
+        self.products = []
 
     def add_product(self, product:str):
         self.products.append(product)
 
     def get_by_letter(self, letter:str):
-        products_by_letter = [product for product in self.products if product[0] == letter]
+        products_by_letter = [product for product in self.products if product[0].lower() == letter.lower()]
         return products_by_letter
 
     def __repr__(self):
-        self.products.sort()
-        return f"Items in tne {self.name} catalogue: \n" + '\n'.join(self.products)
+        sorted_products = sorted(self.products)
+        return f"Items in the {self.name} catalogue: \n" + '\n'.join(sorted_products)
+
 
