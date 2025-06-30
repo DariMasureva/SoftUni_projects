71c482685b2f14ff5ee64ed300a9e89843c48997
Darina Masureva <darimasureva@gmail.com>
2025-06-30 22:57:21 +0300

    corrected solutions

--- fundamentals_solutions_2.0/catalogue.py
+++ fundamentals_solutions_2.0/catalogue.py
@@ -4,14 +4,14 @@ class Catalogue:
         self.name = name
         self.products = []
 
-    def add_product(self, product:str):
-        self.products.append(product)
+    def add_product(self, product_name:str):
+        self.products.append(product_name)
 
-    def get_by_letter(self, letter:str):
-        products_by_letter = [product for product in self.products if product[0].lower() == letter.lower()]
+    def get_by_letter(self, first_letter:str):
+        products_by_letter = [product for product in self.products if product.startswith(first_letter)]
         return products_by_letter
 
     def __repr__(self):
         sorted_products = sorted(self.products)
-        return f"Items in the {self.name} catalogue: \n" + '\n'.join(sorted_products)
+        return f"Items in the {self.name} catalogue:\n" + '\n'.join(sorted_products)
 
--- fundamentals_solutions_2.0/class.py
+++ fundamentals_solutions_2.0/class.py
@@ -7,15 +7,15 @@ class Class:
         self.grades = []
 
     def add_student(self, student:str, grade:float):
-        if len(self.students) < Class.__students_count and student not in self.students:
+        if len(self.students) < Class.__students_count:
             self.students.append(student)
             self.grades.append(grade)
 
     def get_average_grade(self):
         average = 0
         if self.grades:
-            average = sum(self.grades) / len(self.grades)
-        return f"{average:.2f}"
+            average = sum(self.grades) / len(self.students)
+        return float(f"{average:.2f}")
 
     def __repr__(self):
         return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade()}"
