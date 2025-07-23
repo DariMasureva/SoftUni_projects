76828bb12c8a9eef811d716a1305aac500f168be
Darina Masureva <darimasureva@gmail.com>
2025-07-23 16:46:12 +0300

    added solutions

--- fundamentals_solutions/regex_more_ex/html_parser.py
+++ fundamentals_solutions/regex_more_ex/html_parser.py
@@ -3,4 +3,17 @@ import re
 html = input()
 title_pattern = r"<title>(.*)</title>"
 whole_body_pattern = r"<body>(.*)</body>"
-body_content_pattern = r""
+tags_pattern = r"<[^>]*>"
+space_pattern = r"\n|\t|\\n|\\t"
+
+title = re.search(title_pattern, html).group(1)
+body = re.search(whole_body_pattern, html).group(1)
+
+title = re.sub(tags_pattern, "", title)
+body = re.sub(tags_pattern, "", body)
+
+title = re.sub(space_pattern, "", title)
+body = re.sub(space_pattern, "", body)
+
+print("Title:", title)
+print("Content:", body)
